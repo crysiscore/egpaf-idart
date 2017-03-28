d14bc875e7cf51514d49ce4c97be43fc8a27dcad
Agnaldo Samuel <agnaldosamuel3@gmail.com>
2017-03-28 02:13:07 -0700

    Update: Registo de regime de medicamentos (1maio)

---
+++ src/org/ccs/openmrs/migracao/swingreverse/test.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ccs.openmrs.migracao.swingreverse;
+
+import java.io.File;
+
+/**
+ *
+ * @author agnaldo
+ */
+public class test {
+    
+
+        
+    public static void main(String[] args)   throws Exception {
+        // TODO code application logic here
+        
+        Task2 ta = new Task2();
+        
+        String loc = ta.getLogFileLocation();
+        
+        
+    }
+    
+}
---
+++ src/org/celllife/idart/database/hibernate/RegimeTerapeuticoDrugs.java
@@ -0,0 +1,62 @@
+package org.celllife.idart.database.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity (name ="regimeterapeuticodrugs")
+public class RegimeTerapeuticoDrugs implements Comparable<RegimeTerapeuticoDrugs>{
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "regimeid")
+	private RegimeTerapeutico regime;
+	
+	@ManyToOne
+	@JoinColumn(name = "chemicalid")
+	private ChemicalCompound chemicalCompound;
+	
+	public RegimeTerapeuticoDrugs()
+	{}
+	
+	public RegimeTerapeuticoDrugs(RegimeTerapeutico regime,
+			ChemicalCompound chemicalCompound) {
+		this.id = 0;
+		this.regime = regime;
+		this.chemicalCompound = chemicalCompound;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public RegimeTerapeutico getRegime() {
+		return regime;
+	}
+
+	public void setRegime(RegimeTerapeutico regime) {
+		this.regime = regime;
+	}
+
+	public ChemicalCompound getChemicalCompound() {
+		return chemicalCompound;
+	}
+
+	public void setChemicalCompound(ChemicalCompound chemicalCompound) {
+		this.chemicalCompound = chemicalCompound;
+	}
+
+	@Override
+	public int compareTo(RegimeTerapeuticoDrugs o) {
+	    return this.getChemicalCompound().getAcronym().compareToIgnoreCase(o.getChemicalCompound().getAcronym());
+	}
+}
